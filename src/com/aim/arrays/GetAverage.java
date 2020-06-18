28cf7ecf190b779e921094571df021defa00ccca
jteaford <jessteaford@gmail.com>
2020-06-18 16:14:19 -0500

    Initial Commit

--- src/com/aim/arrays/GetAverage.java
+++ src/com/aim/arrays/GetAverage.java
@@ -1,5 +1,7 @@
 package com.aim.arrays;
 
+import java.util.Scanner;
+
 public class GetAverage {
 
     /**
@@ -10,7 +12,35 @@ public class GetAverage {
      * @param args
      */
     public static void main(String[] args) {
+        // Set how many integers
+        int numberOfIntegers = 5;
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter " + numberOfIntegers + " integer values.\r");
+
+        // Declare an array
+        int[] array = new int[numberOfIntegers];
+        for(int i = 0; i < numberOfIntegers; i++) {
+        array[i] = input.nextInt();
+        }
+        input.close();
+
+        // Print all integer values entered by user
+        for(int i = 0; i < array.length; i++){
+            System.out.println("Array element " + i + " is " + array[i]);
+        }
+
+        // Get average of all integers
+        int sum = 0;
+        for(int i = 0; i < array.length; i++){
+            sum += array[i];
+        }
+
+        // Print out sum and average
+        System.out.println("Sum: " + sum);
+        System.out.println("Average: " + sum/numberOfIntegers);
+
+        System.out.println("\n");
     }
 
 }
